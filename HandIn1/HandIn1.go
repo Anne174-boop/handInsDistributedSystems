3f13cb5523e89319718421c86f8eeaaa25d08560
Marius Nunn <mnun@itu.dk>
2026-08-31 15:48:32 +0200

    refactor and new channel structure

--- HandIn1/HandIn1.go
+++ HandIn1/HandIn1.go
@@ -5,29 +5,34 @@ import (
 )
 
 func main() {
-	fmt.Println("CHANGE IN STATE FOR FORK OR PHILOSOPHER")
-	forkChannel := channel()
-	go philosopher(forkChannel)
-}
-
-func channel() chan int {
-	forkChannel := make(chan int, 1) //buffered channel for forks
-	return forkChannel
-}
-
-// goroutine for all philosophers (5)
-func philosopher(forkChannel chan int) {
-	select {
-	case <-forkChannel:
-		forkChannel <- 1 //take fork
-	default:
-		fmt.Println("Philosopher is waiting for fork")
+	//for-loop runs 5 times for 5 philosophers and gives them their ID (0-4)
+	for i := 0; i < 5; i++ {
+		go philosopher(i)                     //goroutine philosopher, program doesn't wait, routine runs when it can
+		go fork(i, forkTable(i), forkHand(i)) //goroutine fork, program doesn't wait, routine runs when it can
 	}
 
 }
 
-// goroutine for all forks (5)
-// forks are its own thread
-func fork() {
+// goroutine for 1 philosopher
+// gets an ID so we can differentiate between philosophers
+func philosopher(ID int) {
+	fmt.Println(ID)
 
 }
+
+func forkTable(ID int) chan int {
+	forkTable := make(chan int)
+	return forkTable
+}
+
+func forkHand(ID int) chan int {
+	forkHand := make(chan int)
+	return forkHand
+}
+
+// forks are its own thread
+func fork(ID int, forkTable chan int, forkHand chan int) {
+
+}
+
+// if fork is free <- send to philosopher, but philosopher has to resive 2 messages to the philosophers next to it.
