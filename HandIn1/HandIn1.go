b78aa8839e0dfdd2e182117758737e609359209d
Marius Nunn <mnun@itu.dk>
2026-08-31 14:54:51 +0200

    setup structure and channel

--- HandIn1/HandIn1.go
+++ HandIn1/HandIn1.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println("CHANGE IN STATE FOR FORK OR PHILOSOPHER")
+	forkChannel := channel()
+	go philosopher(forkChannel)
+}
+
+func channel() chan int {
+	forkChannel := make(chan int, 1) //buffered channel for forks
+	return forkChannel
+}
+
+// goroutine for all philosophers (5)
+func philosopher(forkChannel chan int) {
+	select {
+	case <-forkChannel:
+		forkChannel <- 1 //take fork
+	default:
+		fmt.Println("Philosopher is waiting for fork")
+	}
+
+}
+
+// goroutine for all forks (5)
+// forks are its own thread
+func fork() {
 
 }
